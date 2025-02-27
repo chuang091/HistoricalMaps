f3b2fa3a97d05986a013e84d72428bb55d16be4b
chuang091 <thomas@ultralytics.com>
2025-02-28 00:21:42 +0800

    Update package.json and pages/index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,1 +1,1 @@
-<template><div class="text-center p-10"><h1 class="text-4xl font-bold">Hello, Nuxt!</h1></div></template>
+<template><div class="text-center p-10"><h1 class="text-4xl font-bold">Hello, Nuxt! YOYOYO</h1></div></template>
